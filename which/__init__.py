ecaf7a85fbc1cc29871888e6a889e91d243e48fc
Pierre de Buyl <pdebuyl@pdebuyl.be>
2017-03-13 21:10:39 +0100

    move which function to separate file
    This change is made to avoid imported module names appearing in the
    which namespace.

--- which/__init__.py
+++ which/__init__.py
@@ -2,28 +2,7 @@
 A module to find the location of other modules. The main use is to diagnose
 installation problems.
 """
-from __future__ import print_function
-import imp
-import sys
+from .which import which
 
 __version__ = '0.2-dev'
-
-
-def which(module, sys_path=False, module_version=False):
-    print("Python executable:", sys.executable)
-    if sys_path:
-        print("System path:", sys.path)
-    try:
-        file, pathname, description = imp.find_module(module)
-    except:
-        print("""Module "{}" not found""".format(module))
-        return
-
-    print("""Module "{}" found at location: {}""".format(module,
-                                                         pathname))
-    if module_version:
-        mod = imp.load_module(module, file, pathname, description)
-        if hasattr(mod, '__version__'):
-            print("""{} version: {}""".format(module, mod.__version__))
-        else:
-            print('Module has no __version__ information')
+__all__ = ['which']
---
+++ which/which.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import imp
+import sys
+
+
+def which(module, sys_path=False, module_version=False):
+    print("Python executable:", sys.executable)
+    if sys_path:
+        print("System path:", sys.path)
+    try:
+        file, pathname, description = imp.find_module(module)
+    except:
+        print("""Module "{}" not found""".format(module))
+        return
+
+    print("""Module "{}" found at location: {}""".format(module,
+                                                         pathname))
+    if module_version:
+        mod = imp.load_module(module, file, pathname, description)
+        if hasattr(mod, '__version__'):
+            print("""{} version: {}""".format(module, mod.__version__))
+        else:
+            print('Module has no __version__ information')
