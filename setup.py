5d088fce9faf4d30e5ad84c9b42255c6cc08f227
Pierre de Buyl <pdebuyl@pdebuyl.be>
2017-03-14 18:39:17 +0100

    add metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,23 @@ from setuptools import setup
 
 from whych import __version__
 
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
 setup(name="whych",
       version=__version__,
       description="Find the location for a given module",
+      long_description=readme,
       author="Pierre de Buyl",
-      author_email="pdebuyl at pdebuyl dot be",
+      author_email="pdebuyl@pdebuyl.be",
       license="BSD",
       url="https://github.com/pdebuyl/whych",
       packages=["whych"],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Utilities',
+      ],
       )
